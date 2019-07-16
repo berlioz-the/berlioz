cd36b1d79fac7fa1b17632cbc55be726914bc32f
berlioz <berlioz-cloud@gmail.com>
2019-07-16 13:07:51 -0700

    Upgraded JOI package.

---
+++ scripts/postinstall.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('')
+console.log('+---------------------------------------------+')
+console.log('|                                             |')
+console.log('|  Berlioz is now installed!                  |')
+console.log('|                                             |')
+console.log('|  See our training materials at:             |')
+console.log('|      https://docs.berlioz.cloud/training    |')
+console.log('|                                             |')
+console.log('+---------------------------------------------+')
