2ca2c53284295bd8986af298364f9cc0206d046c
berlioz <berlioz-cloud@gmail.com>
2019-08-03 01:00:57 +0100

    Berlioz CLI Release.
    Modified Areas:
    git-release.sh
    
    Files Changed:
    M  git-release.sh

--- git-release.sh
+++ git-release.sh
@@ -51,11 +51,11 @@ if [ $retval -ne 0 ]; then
     exit 4
 fi
 
-# echo "Pushing change to remote $CURR_BRANCH_NAME..."
-# git push origin $CURR_BRANCH_NAME
-# retval=$?
-# if [ $retval -ne 0 ]; then
-#     echo "ERROR: Failed to push the branch."
-#     exit 4
-# fi
+echo "Pushing change to remote $CURR_BRANCH_NAME..."
+git push origin $CURR_BRANCH_NAME
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo "ERROR: Failed to push the branch."
+    exit 4
+fi
 
